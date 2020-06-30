b24081b6f38933bd954d32e9a4e3e259174e0fad
hungodinh <hung.dinh@furo.io>
2020-06-30 13:21:37 +1000

    Update php-q1.php

--- php-q1.php
+++ php-q1.php
@@ -1,3 +1,4 @@
+<?php
 $str1 = 'yabadabadoo';
 $str2 = 'yaba';
 if (strpos($str1,$str2)) {
@@ -5,3 +6,4 @@ if (strpos($str1,$str2)) {
 } else {
     echo "\"" . $str1 . "\" does not contain \"" . $str2 . "\"";
 }
+?>
